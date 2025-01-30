24e0b8ad407b8759e491c6b791f36085f0d70364
roflmaostc <fxw+git@mailbox.org>
2025-01-30 20:37:36 +0100

    Introduce a weight term to the object and void region

--- src/drtvam/loss.py
+++ src/drtvam/loss.py
@@ -55,25 +55,39 @@ class L2Loss(Loss):
     def eval(self, x, target):
         return dr.square(x - target)
 
+
 class ThresholdedLoss(Loss):
     """
     Thresholded loss following Wechsler et al 2024.
 
+    The loss is defined as:
+    L(x) = weight_object * relu(tu - x)^K + weight_void * relu(x - tl)^K + relu(x - 1)^K
+
+    where:
+    - x is the intensity distribution in the printing region
+    - tu is the upper threshold
+    - tl is the lower threshold
+    - weight_object and weight_void are the weights for the object and void regions
+    - K is the exponent for the loss function
     """
     def __init__(self, props):
         super().__init__(props)
         self.K = props.get('K', 2)
         self.tl = props.get('tl', 0.9)
         self.tu = props.get('tu', 0.95)
+        # put a different weight for the object and void regions
+        self.weight_object = props.get('weight_object', 1)
+        self.weight_void = props.get('weight_void', 1)
+
 
         if self.tl >= self.tu:
             raise ValueError(f"[ThresholdedLoss] Lower threshold ({self.tl}) must be smaller than upper threshold ({self.tu})")
 
     def eval_in(self, x):
-        return relu(self.tu - x)**self.K + relu(x - 1.)**self.K
+        return self.weight_object * relu(self.tu - x)**self.K + relu(x - 1.)**self.K
 
     def eval_out(self, x):
-        return relu(x - self.tl)**self.K
+        return self.weight_void * relu(x - self.tl)**self.K
 
     def eval(self, x, target):
         # target should be a binary inside/outside mask
